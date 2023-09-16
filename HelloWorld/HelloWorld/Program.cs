8fd0d80e5045c776489b376e066bf9e1e43f7801
Warrior Divs <Warriordivs@gmail.com>
2023-09-16 23:17:07 +0530

    Explained code

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        //WriteLine() and Write() are the functions to print something on the console
         Console.WriteLine("Hello World");
         //Below line is added to make the output screen wait for user to enter a key so that we can see the output
         Console.ReadKey();
