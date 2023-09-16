b260f0823e37a9993955122ffa328caa1255c3d1
Warrior Divs <Warriordivs@gmail.com>
2023-09-16 23:01:48 +0530

    ReadKey

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        //Below line is added to make the output screen wait for user to enter a key so that we can see the output
+        Console.ReadKey();
+    }
+}
